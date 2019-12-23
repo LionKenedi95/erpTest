ea648c403436432314b6c67ac9428973457249cb
Vadim Demchenko <lionkenedi95@gmail.com>
2019-12-23 22:34:42 +0300

    Prepare project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CustomForm/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CustomForm from './components/CustomForm.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    CustomForm
   }
 }
 </script>
---
+++ src/components/CustomForm.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <div>
+            <input
+                v-model="input1"
+                type="number"
+            >
+        </div>
+        <div>
+            <input
+                v-model="input2"
+                type="number"
+            >
+        </div>
+        <div>
+            {{ output }}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            func: {
+                type: Function,
+                default: (a,b) => a + b,
+            },
+        },
+        data() {
+            return {
+                input1: 0,
+                input2: 0,
+            };
+        },
+        computed: {
+            preparedInput1() {
+                return this.input1 ? parseInt(this.input1) : 0;
+            },
+            preparedInput2() {
+                return this.input2 ? parseInt(this.input2) : 0;
+            },
+            output() {
+                return this.func(this.preparedInput1, this.preparedInput2);
+            },
+        }
+    }
+</script>
+    
+<style>
+    
+</style>
